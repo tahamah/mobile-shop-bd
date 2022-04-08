f97490709e67b22ffb093dcc5009a7255ee2a2ed
unknown <taha.iu.bd@gmail.com>
2022-04-08 10:19:43 +0600

    btn-link-add

--- js/app.js
+++ js/app.js
@@ -1,30 +1,30 @@
 //error
-const error = status => {
+const error = (status) => {
     document.getElementById('not-found').style.display = status
 }
 
 //spinner
-const spinner = status => {
+const spinner = (status) => {
     document.getElementById('loading-spinner').style.display = status
 }
 
 //clear text content
-const clearTextContent = id => {
+const clearTextContent = (id) => {
     document.getElementById(id).textContent = ''
 }
 
 //clear input value
-const clearInputValue = id => {
+const clearInputValue = (id) => {
     document.getElementById(id).value = ''
 }
 
 //load data
 const lodeData = () => {
     //clear previous content
-    document.getElementById('single-phone').style.display = "none"
+    document.getElementById('single-phone').style.display = 'none'
     spinner('block')
 
-    //error check, spinner && data load 
+    //error check, spinner && data load
     const inputValue = document.getElementById('inputField').value
     if (inputValue === '') {
         clearTextContent('search-result-container')
@@ -37,14 +37,14 @@ const lodeData = () => {
         error('none')
         const url = `https://openapi.programming-hero.com/api/phones?search=${inputValue}`
         fetch(url)
-            .then(res => res.json())
-            .then(data => displayData(data.data.slice(0, 20)))
+            .then((res) => res.json())
+            .then((data) => displayData(data.data.slice(0, 20)))
         clearInputValue('inputField')
     }
 }
 
 //display Data
-const displayData = phones => {
+const displayData = (phones) => {
     //error check , spinner && display data
     if (phones.length === 0) {
         clearTextContent('search-result-container')
@@ -57,8 +57,10 @@ const displayData = phones => {
         clearTextContent('search-result-container')
 
         //display data
-        const searchResultContainer = document.getElementById('search-result-container')
-        phones.forEach(phone => {
+        const searchResultContainer = document.getElementById(
+            'search-result-container'
+        )
+        phones.forEach((phone) => {
             const div = document.createElement('div')
             div.className = 'col'
             div.innerHTML = `
@@ -84,10 +86,8 @@ const displayData = phones => {
     }
 }
 
-
-
-// load Single phone 
-const lodeSinglePhone = phoneId => {
+// load Single phone
+const lodeSinglePhone = (phoneId) => {
     //clear previous && spinner
     clearTextContent('single-phone')
     spinner('block')
@@ -96,14 +96,12 @@ const lodeSinglePhone = phoneId => {
     document.getElementById('single-phone').style.display = 'block'
     const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`
     fetch(url)
-        .then(res => res.json())
-        .then(data => displaySinglePhone(data.data))
-
+        .then((res) => res.json())
+        .then((data) => displaySinglePhone(data.data))
 }
 
-
 // display Single Phone
-const displaySinglePhone = phone => {
+const displaySinglePhone = (phone) => {
     //clear previous
     clearTextContent('single-phone')
 
@@ -111,32 +109,50 @@ const displaySinglePhone = phone => {
     const singlePhoneContainer = document.getElementById('single-phone')
     const div = document.createElement('div')
     div.innerHTML = `
-                <div class="row g-0">
+                <div id="header" class="row g-0">
                     <div class="col-md-4 my-auto">
-                        <img src="${phone.image}" class="img-fluid rounded-start my-auto p-3" alt="...">
+                        <img src="${
+                            phone.image
+                        }" class="img-fluid rounded-start my-auto p-3" alt="...">
                     </div>
                     <div class="col-md-8 my-auto">
                         <div class="card-body">
                             <h5 class="card-title">
-                                 <span class='fw-bolder'> Name:</span>  ${phone.name}
+                                 <span class='fw-bolder'> Name:</span>  ${
+                                     phone.name
+                                 }
                             </h5>
                             <h6 class="card-text">
-                                <span class='fw-bolder'> ReleaseDate:</span> ${phone.releaseDate ? phone.releaseDate:'No Release Date Found'}
+                                <span class='fw-bolder'> ReleaseDate:</span> ${
+                                    phone.releaseDate
+                                        ? phone.releaseDate
+                                        : 'No Release Date Found'
+                                }
                             </h6>
                             <p>
-                                <span class='fw-bolder'> ChipSet: </span> ${phone.mainFeatures.chipSet}
+                                <span class='fw-bolder'> ChipSet: </span> ${
+                                    phone.mainFeatures.chipSet
+                                }
                             </p>
                             <p>
-                                <span class='fw-bolder'> Display Size: </span> ${phone.mainFeatures.displaySize}
+                                <span class='fw-bolder'> Display Size: </span> ${
+                                    phone.mainFeatures.displaySize
+                                }
                             </p>
                             <p>
-                                <span class='fw-bolder'> Memory:</span> ${phone.mainFeatures.memory}
+                                <span class='fw-bolder'> Memory:</span> ${
+                                    phone.mainFeatures.memory
+                                }
                             </p>
                             <p>
-                                <span class='fw-bolder'> Storage:</span> ${phone.mainFeatures.storage}
+                                <span class='fw-bolder'> Storage:</span> ${
+                                    phone.mainFeatures.storage
+                                }
                             </p>
                             <p>
-                                <span class='fw-bolder'> Sensors:</span> ${phone.mainFeatures.sensors}
+                                <span class='fw-bolder'> Sensors:</span> ${
+                                    phone.mainFeatures.sensors
+                                }
                             </p>
                             <div>
                                 <div id='undefined-others' > 
@@ -145,12 +161,24 @@ const displaySinglePhone = phone => {
                 
                                 <div id='defined-others' > 
                                     <span class='fw-bolder'> Others:</span> </br>
-                                    <span class='fw-bolder'> Bluetooth:</span> ${phone.others?.Bluetooth}
-                                    <span class='fw-bolder'> GPS:</span> ${phone.others?.GPS} 
-                                    <span class='fw-bolder'> NFC:</span> ${phone.others?.NFC}
-                                    <span class='fw-bolder'> Radio:</span> ${phone.others?.Radio}
-                                    <span class='fw-bolder'> USB:</span> ${phone.others?.USB}
-                                    <span class='fw-bolder'> WLAN:</span> ${phone.others?.WLAN}   
+                                    <span class='fw-bolder'> Bluetooth:</span> ${
+                                        phone.others?.Bluetooth
+                                    }
+                                    <span class='fw-bolder'> GPS:</span> ${
+                                        phone.others?.GPS
+                                    } 
+                                    <span class='fw-bolder'> NFC:</span> ${
+                                        phone.others?.NFC
+                                    }
+                                    <span class='fw-bolder'> Radio:</span> ${
+                                        phone.others?.Radio
+                                    }
+                                    <span class='fw-bolder'> USB:</span> ${
+                                        phone.others?.USB
+                                    }
+                                    <span class='fw-bolder'> WLAN:</span> ${
+                                        phone.others?.WLAN
+                                    }   
                                 </div>
                             </div>
                         </div>
